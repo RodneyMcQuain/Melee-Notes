b09a2fd033d8bb8b1851c18b8223eb5542ebb573
Rodney McQuain <rodneymcquain95@gmail.com>
2018-07-01 15:21:18 -0400

    Added a new test for testing sets with the all specification

--- src/test/StatTest.java
+++ src/test/StatTest.java
@@ -2,6 +2,8 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 import main.StatDao;
@@ -11,6 +13,7 @@ class StatTest {
 
 	@Test
 	void specifyAllSetsFieldsTest() {
+		SQLiteTestData.deleteDatabaseFile();
 		SQLiteTestData.buildEntireDatabase();
 
 		StatDao test = new StatDaoImpl();
@@ -24,6 +27,25 @@ class StatTest {
 
 		assertEquals(3, setsWon);
 		assertEquals(3, setsLost);
+	}
+	
+	@Test
+	void allSetsFieldsTest() {
+		SQLiteTestData.deleteDatabaseFile();
+		SQLiteTestData.buildEntireDatabase();
+
+		StatDao test = new StatDaoImpl();
+		String playerSql = "LIKE '%' OR tag IS NULL OR tag = ' '";
+		String formatSql = "LIKE '%' OR type IS NULL OR type = ' '";
+		String typeSql = "LIKE '%' OR format IS NULL OR format = ' '";
+		String startDate = "2000-01-01";
+		String endDate = LocalDate.now().toString();
+
+		int setsWon = test.calculateSetsWon(playerSql, formatSql, typeSql, startDate, endDate);
+		int setsLost = test.calculateSetsLost(playerSql, formatSql, typeSql, startDate, endDate);
+
+		assertEquals(7, setsWon);
+		assertEquals(8, setsLost);
 	}
 
 }
